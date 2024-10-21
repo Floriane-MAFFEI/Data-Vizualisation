398f4f35421403f5c35ab55bc710e2a5ce9075d4
Floriane-MAFFEI <floriane.oclock@gmail.com>
2024-10-21 19:16:45 +0200

    FilteringService injection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,13 +4,14 @@ import { AppService } from './app.service';
 import { Transport } from '@nestjs/microservices';
 import { FakeProbeService } from './services/fake-probe.service';
 import { DataController } from './data.controller';
+import { FilteringService } from './services/filtering.service';
 
 @Module({
   imports: [],
   // controller to instantiate for the operation of the application
   controllers: [DataController],
   // to be injected for the operation of the application
-  providers: [AppService, FakeProbeService],
+  providers: [AppService, FakeProbeService, FilteringService],
 })
 export class AppModule {
   // For configure microservice at the root of the application
