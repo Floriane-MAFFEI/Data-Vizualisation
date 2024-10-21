719025fe1569dc69bb2c67e0a0c6080726bc9636
Floriane-MAFFEI <floriane.oclock@gmail.com>
2024-10-21 16:04:09 +0200

    Microservice configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
+
+  // Initialize the NestJS application
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Start the microservice in 3001 port
+  const microservice = app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.TCP,
+    options: {
+      port: 3001,
+    },
+  });
+
+  await app.startAllMicroservices();
+  await app.listen(3000); // HTTP Server in another port
 }
+
+// Intitialize the application, connect and start the microservice and listen for the HTTP request
 bootstrap();
