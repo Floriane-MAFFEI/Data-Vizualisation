6efa2ff5223b46e10dcda7a35edf4f01ff04965f
Floriane-MAFFEI <floriane.oclock@gmail.com>
2024-10-22 17:19:20 +0200

    Creating the route for filter data

--- backend/src/data.controller.ts
+++ backend/src/data.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { FakeProbeService } from './services/fake-probe.service';
+import { FilteringService } from './services/filtering.service';
+import { Observable, forkJoin } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Controller('data')
 export class DataController {
-
   constructor(
     private readonly fakeProbeService: FakeProbeService,
+    private readonly filteringService: FilteringService,
   ) {
-    // Start the data generation service when calling the DataController
     this.fakeProbeService.startEmittingRandomNumbers();
   }
 
@@ -17,5 +19,21 @@ export class DataController {
     const rawData = this.fakeProbeService.getRawData();
     // console.log('Raw data:', rawData); // For debugging
     return { data: rawData };
+  }
+
+  // Path with GET to return filtered data
+  @Get('filtered')
+  getFilteredData(): Observable<any> {
+    // Retrieve raw data form the FakeProbeService
+    const rawData = this.fakeProbeService.getRawData();
+
+    // For each number (rawData), filter via filterNumber of the filteringService
+    const filteredData = rawData.map(num => this.filteringService.filterNumber(num));
+
+    // Wait for filteredData to provide results
+    return forkJoin(filteredData).pipe(
+      // Create an object that contains the filtered data
+      map(filteredData => ({ data: filteredData }))
+    );
   }
 }
