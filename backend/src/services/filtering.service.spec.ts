62700bc98b892c5456b49756c91ea616424c854c
Floriane-MAFFEI <floriane.oclock@gmail.com>
2024-10-23 11:39:09 +0200

    Create test of generate fibonacci sequence with a limit

--- backend/src/services/filtering.service.spec.ts
+++ backend/src/services/filtering.service.spec.ts
@@ -1,16 +1,40 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 import { FilteringService } from './filtering.service';
+import { TestingModule, Test } from '@nestjs/testing';
 
-describe('Service: Filtering', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [FilteringService]
-    });
+describe('FilteringService', () => {
+  let service: FilteringService;
+
+  // Configure before each tests (new instance)
+  beforeEach(async () => {
+    // Create a test module
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [FilteringService],
+    }).compile();
+
+    service = module.get<FilteringService>(FilteringService);
   });
 
-  it('should ...', inject([FilteringService], (service: FilteringService) => {
-    expect(service).toBeTruthy();
-  }));
+  it('should generate the Fibonacci sequence up to a limit : 100 ', () => {
+    const limit = 100;
+    const expectedSequence = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+
+    const generatedSequence = service.generateFibonacci(limit);
+
+    expect(generatedSequence).toEqual(expectedSequence)
+
+  });
+
+  it('should generate the Fibonacci sequence up to a limit : 250 ', () => {
+    const limit = 250;
+    const expectedSequence = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233];
+
+    const generatedSequence = service.generateFibonacci(limit);
+
+    expect(generatedSequence).toEqual(expectedSequence)
+
+  });
+
 });
