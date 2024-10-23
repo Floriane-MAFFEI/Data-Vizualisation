c74129101972744b261f612b6eb5830e5ba31823
Floriane-MAFFEI <floriane.oclock@gmail.com>
2024-10-23 11:09:38 +0200

    Correction of the filterNumber function with integration of a findNearMoreFibonacci method and creation of the findNearMoreFibonacci function

--- backend/src/services/filtering.service.ts
+++ backend/src/services/filtering.service.ts
@@ -7,6 +7,7 @@ export class FilteringService {
   private fibonacciSequence: number[];
 
   constructor() {
+    // Generate the Fibonacci Sequence up to 100
     this.fibonacciSequence = this.generateFibonacci(100);
   }
 
@@ -34,23 +35,39 @@ export class FilteringService {
   filterNumber(num: number): Observable<any> {
 
     //  Return to 100 if the number is greater than or equal to 100
+    if (num >= 100) {
+      num = 100;
+    }
+
+    // Return the near more fibonacci sequence number via map
     return of(num).pipe(
-      map(num => {
-        if (num >= 100) {
-          return 100;
-        } else {
-          return "OK"
-        }
-      })
-    )
-    //TODO Return the near more fibonacci sequence number via map
+      map(num => this.findNearMoreFibonacci(num)))
 
   }
 
-  // TODO: Find the near more Fibonacci number
-  // TODO: Initialize the counter (near more)
-  // TODO: Loop through the Fibonacci sequence
-  // TODO: Compare the number with each Fibonacci number
-  // TODO: If the Fibonacci number is greater than num, keep it as a candidate
-  // TODO: If the Fibonacci number is less than or equal to num, update the near more
+  // Find the near more Fibonacci number
+  private findNearMoreFibonacci(num: number): number {
+    // Initialize the nearMore of type number or undefined
+    let nearMore: number | undefined;
+
+    // Loop through the Fibonacci sequence
+    for (const fibonacciNum of this.fibonacciSequence) {
+      // console.log("fibnum:", fibonacciNum); // for debugging
+
+      // Compare the number with each Fibonacci number
+      if (fibonacciNum >= num) {
+        // If the Fibonacci number is greater than num, 
+        // keep it as a candidate
+        nearMore = fibonacciNum;
+        // console.log("nearMore:", nearMore) // for debugging
+
+        // Exit the loop after finding the more near
+        break;
+      }
+    }
+
+    return nearMore !== undefined ? nearMore : 100; // Return  if not number found
+
+  }
+
 }
